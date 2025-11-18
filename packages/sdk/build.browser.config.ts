26b0d159ad67593615cb65e255f523b5d32402e0
Kevin Kern <hello@regenrek.at>
2025-11-18 13:50:11 +0100

    chore: add missing browser build config

---
+++ packages/sdk/build.browser.config.ts
@@ -0,0 +1,61 @@
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  // Browser-specific entry point
+  entries: ["./src/browser"],
+
+  // Output to separate directory
+  outDir: "dist-browser",
+
+  // Target browser environment
+  rollup: {
+    treeshake: true,
+    emitCJS: false,
+    esbuild: {
+      target: "es2022",
+      platform: "browser",
+      format: "esm",
+    },
+    output: {
+      inlineDynamicImports: false,
+    },
+    dts: {
+      respectExternal: false,
+    },
+  },
+
+  // Clean output directory
+  clean: true,
+
+  // Generate declarations
+  declaration: true,
+
+  // Externals - browser doesn't have Node.js built-ins
+  externals: [
+    // Node built-ins not available in browser
+    "node:os",
+    "node:path",
+    "node:fs",
+    "node:fs/promises",
+    "node:crypto",
+    "node:buffer",
+    "node:stream",
+    "node:util",
+    "node:zlib",
+    "node:child_process",
+    "os",
+    "path",
+    "fs",
+    "fs/promises",
+    "crypto",
+    "buffer",
+    "stream",
+    "util",
+    "zlib",
+    "child_process",
+  ],
+
+  // Fail on warnings to catch issues early
+  failOnWarn: false,
+});
+
